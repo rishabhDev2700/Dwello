1fca17c1055b92bf5e2728909182255421b26476
rishabhDev2700 <rishavkumar2700@gmail.com>
2023-01-09 23:53:10 +0530

    removed some files from gitignore

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'pacifico': ['Pacifico', 'cursive'],
+      'oxygen':['Oxygen', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
